d922f19f783bbc6a5548f776f1491f2f14187ff5
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-08 13:59:36 +0500

    Replace get with put in route file

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -24,7 +24,7 @@ router.route('/:id/players')
   .get(getEventWithPlayers)
 
   router.route('/:eventId/players/:playerId/availability')
-  .get(updatePlayerAvailability)
+  .put(updatePlayerAvailability)
 
 
 export default router;
