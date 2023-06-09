48c442f4ecb7617c547a9e087be7dc37628e1e58
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-09 13:51:52 +0500

    Add smart-coach-api endpoint in allowed origins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   'http://localhost:5000',
   'http://localhost:8000',
-  'https://smart-coach-api.vercel.app',
+  'https://smart-coach-api.vercel.app/',
   'https://smart-coach-api.vercel.app/api',
 ]
 
