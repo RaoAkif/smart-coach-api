da8aab9cc4b43e9bbc070c2758c715de21508fd4
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-14 12:43:12 +0500

    Add middlewares

---
+++ src/middleware/loginLimiter.ts
@@ -0,0 +1,10 @@
+import rateLimit from 'express-rate-limit';
+
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // Limit each IP to 5 login requests per `window` per minute
+  message:
+    'Too many login attempts from this IP, please try again after a 60 second pause',
+});
+
+export default loginLimiter;
---
+++ src/middleware/verifyJWT.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+
+interface AuthenticatedRequest extends Request {
+    coach?: string;
+    roles?: string[];
+}
+
+const verifyJWT = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (typeof authHeader !== 'string' && !(authHeader instanceof Array)) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const headerValue = typeof authHeader === 'string' ? authHeader : authHeader[0];
+
+    if (!headerValue.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const token = headerValue.split(' ')[1];
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET as Secret,
+        (err, decoded) => {
+            if (err || !decoded || typeof decoded !== 'object' || !('CoachInfo' in decoded)) {
+                return res.status(403).json({ message: 'Forbidden' });
+            }
+
+            const decodedPayload = decoded as JwtPayload;
+            req.coach = decodedPayload.CoachInfo.username;
+            req.roles = decodedPayload.CoachInfo.roles;
+            next();
+        }
+    );
+};
+
+module.exports = verifyJWT;
