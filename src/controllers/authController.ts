87026859eb5f8fe3c6a3aa3fe6b35b0b90d083ea
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-14 11:20:34 +0500

    Add accessToken and refreshToken logic

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
+import jwt, { Secret } from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
+
+console.log('ACCESS_TOKEN_SECRET:', process.env.ACCESS_TOKEN_SECRET);
+console.log('REFRESH_TOKEN_SECRET:', process.env.REFRESH_TOKEN_SECRET);
 
 // @desc Login a new coach
 // @route POST /coaches
@@ -17,7 +21,31 @@ export const loginCoach = async (req: Request, res: Response, next: NextFunction
       const validPassword = await (req.body.password, coach.password);
 
       if (validPassword) {
-        res.json(coach);
+        const accessToken = jwt.sign(
+          {
+            "CoachInfo": {
+              "coachId": coach.id
+              // "username": coach.username
+            }
+          },
+          process.env.ACCESS_TOKEN_SECRET as Secret,
+          { expiresIn: '7d' }
+        )
+    
+        const refreshToken = jwt.sign(
+          { "username": coach.username },
+          process.env.REFRESH_TOKEN_SECRET as Secret,
+          { expiresIn: '28d' }
+        )
+    
+        res.cookie('jwt', refreshToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none',
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+        })
+    
+        res.json({ accessToken, id: coach.id })
       } else {
         res.json("Wrong Credentials");
       }
