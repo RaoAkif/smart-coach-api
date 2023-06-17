8d87991ad6f60558f5a6390bd3186bcf18df07a8
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-17 22:27:08 +0500

    Update swagger.ts

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,5 +1,10 @@
+import express, { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc, { Options, SwaggerDefinition } from 'swagger-jsdoc';
 
+const app: Express = express();
+
+/// Swagger documentation
 const options: Options = {
   definition: {
     openapi: '3.0.0',
@@ -15,9 +20,12 @@ const options: Options = {
       },
     ],
   },
-  apis: ['./src/routes/*.ts'], // Replace with your route files
+  apis: ['./src/routes/*.ts'],
 };
 
 const swaggerSpec: SwaggerDefinition = swaggerJSDoc(options) as SwaggerDefinition;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 export default swaggerSpec;
