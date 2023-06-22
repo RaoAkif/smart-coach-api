ee5fae37d6cf601850b9034395f2a62e97293412
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 22:19:29 +0500

    Update allowedOrigins.ts

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -3,7 +3,8 @@ const allowedOrigins = [
   'http://localhost:8000',
   'http://localhost:5173',
   'https://smart-coach-api.vercel.app',
-  'https://smart-coach.netlify.app'
+  'https://smart-coach.netlify.app',
+  'https://smart-coach-api.vercel.app'
 ];
 
 export default allowedOrigins;
